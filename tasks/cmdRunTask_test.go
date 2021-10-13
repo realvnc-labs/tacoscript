44a79273c07b0d56990e4e4acabc9cef166dffd9
Martin Lindhe <martin@ubique.se>
2021-10-13 18:31:58 +0200

    sca

--- tasks/cmdRunTask_test.go
+++ tasks/cmdRunTask_test.go
@@ -363,36 +363,6 @@ func TestTaskExecution(t *testing.T) {
 	}
 }
 
-func AssertCmdsPartiallyMatch(t *testing.T, expectedCmds []string, actualExecutedCmds []*exec.Cmd) {
-	notFoundCmds := make([]string, 0, len(expectedCmds))
-
-	executedCmdStrs := make([]string, 0, len(actualExecutedCmds))
-	for _, actualCmd := range actualExecutedCmds {
-		executedCmdStrs = append(executedCmdStrs, actualCmd.String())
-	}
-
-	for _, expectedCmdStr := range expectedCmds {
-		cmdFound := false
-		for _, actualCmdStr := range executedCmdStrs {
-			if strings.HasSuffix(actualCmdStr, expectedCmdStr) {
-				cmdFound = true
-				break
-			}
-		}
-		if !cmdFound {
-			notFoundCmds = append(notFoundCmds, expectedCmdStr)
-		}
-	}
-
-	assert.Empty(
-		t,
-		notFoundCmds,
-		"was not able to find following expected commands '%s' in the list of executed commands '%s'",
-		strings.Join(notFoundCmds, ", "),
-		strings.Join(executedCmdStrs, ", "),
-	)
-}
-
 func TestCmdRunTaskValidation(t *testing.T) {
 	testCases := []struct {
 		Task          CmdRunTask
