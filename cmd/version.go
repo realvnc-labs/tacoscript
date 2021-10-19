688875abfe317296b31e8dec5eb9903f5cad7bfd
Martin Lindhe <martin@ubique.se>
2021-10-19 13:14:41 +0200

    fix version tag embedding

--- cmd/version.go
+++ cmd/version.go
@@ -2,36 +2,17 @@ package cmd
 
 import (
 	"fmt"
-	"time"
-
-	"github.com/spf13/cobra"
 )
 
 var (
-	Version   = "0.0.1"
+	Version   = ""
 	BuildTime = ""
 	GitCommit = ""
-	GitRef    = ""
 )
-
-func init() {
-	rootCmd.AddCommand(versionCmd)
-	BuildTime = time.Now().String()
-}
-
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the version number of Tacoscript",
-	Long:  `All software has versions. This is Tacoscript's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version())
-	},
-}
 
 func version() string {
 	return fmt.Sprintf(`Version: %s
 Build time: %s
 Git commit: %s
-Git ref: %s
-`, Version, BuildTime, GitCommit, GitRef)
+`, Version, BuildTime, GitCommit)
 }
